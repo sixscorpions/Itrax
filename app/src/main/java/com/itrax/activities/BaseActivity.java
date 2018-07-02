e2e2e60fea096c78adc8392dfcf48263d1e97cb8
Shankar Pilli <sandy0210>
2018-07-02 08:30:24 +0530

    fixed back issue

--- app/src/main/java/com/itrax/activities/BaseActivity.java
+++ app/src/main/java/com/itrax/activities/BaseActivity.java
@@ -3,11 +3,16 @@ package com.itrax.activities;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+
+import com.itrax.R;
+import com.itrax.fragments.HomeFragment;
+import com.itrax.utils.Utility;
 
 
 /**
@@ -15,6 +20,8 @@ import android.widget.EditText;
  **/
 
 public class BaseActivity extends AppCompatActivity {
+
+    private int mClosePressCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +52,33 @@ public class BaseActivity extends AppCompatActivity {
         if (activity != null && activity.getWindow() != null && activity.getWindow().getDecorView() != null) {
             InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+        }
+    }
+
+    public void onBackPressed() {
+        try {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            if (fragmentManager.getBackStackEntryCount() > 0) {
+                FragmentManager.BackStackEntry backStackEntry = fragmentManager
+                        .getBackStackEntryAt(fragmentManager
+                                .getBackStackEntryCount() - 1);
+                Utility.showLog("BackStackEntry Name", backStackEntry.getName());
+                if (backStackEntry.getName().equalsIgnoreCase(HomeFragment.TAG)) {
+                    mClosePressCount++;
+                    if (mClosePressCount > 1) {
+                        finishAffinity();
+                    } else {
+                        Utility.showToastMessage(getApplicationContext(), getResources().getString(R.string.press_again_to_exit));
+                    }
+                } else {
+                    super.onBackPressed();
+                }
+            } else {
+                super.onBackPressed();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            getFragmentManager().popBackStack();
         }
     }
 
